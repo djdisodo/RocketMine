4d49047a302bb9270b71c495ea68ba91bf477629
djdisodo <djdisodo@gmail.com>
2020-08-13 05:07:38 +0900

    working

--- src/network/mcpe/raknet/raknet_interface.rs
+++ src/network/mcpe/raknet/raknet_interface.rs
@@ -3,14 +3,14 @@ use crate::Server;
 use crate::network::network::Network;
 use crate::network::mcpe::raknet::RaknetServer;
 use crate::network::mcpe::network_session::NetworkSession;
-use raknet_rs::server::ipc::RaknetToUserThreadMessageReceiver;
 use std::io::Read;
+use raknet_rs::server::ipc::RaknetToUserThreadEventReceiver;
 
-pub struct RaknetInterface<R: Read> {
-	server: Arc<Mutex<Server>>,
+pub struct RaknetInterface<'a> {
+	server: &'a Server,
 	network: Arc<Mutex<Network>>,
 	raknet_server_id: u64,
 	raknet: Arc<Mutex<RaknetServer>>,
 	sessions: Vec<NetworkSession>,
-	event_receiver: RaknetToUserThreadMessageReceiver<R>,
+	event_receiver: RaknetToUserThreadEventReceiver,
 }
